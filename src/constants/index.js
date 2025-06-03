b9d988770bc1dd659d28bb83afd7adb86220234c
Shreyalsinh Raj <131889844+Shreyal216@users.noreply.github.com>
2025-06-03 14:08:36 +0530

    Update index.js

--- src/constants/index.js
+++ src/constants/index.js
@@ -1,7 +1,7 @@
 import project1 from "../assets/projects/project-1.jpg";
 import mouse from "../assets/projects/mouse.png";
 import portfolio from "../assets/projects/portfolio.png";
-// import project4 from "../assets/projects/project-4.jpg";
+
 
 export const HERO_CONTENT = `I am a passionate frontend developer skilled in creating dynamic, responsive, and user-friendly web interfaces. Proficient in Html, Css, Java, Javascript, Sql, React and Node.js, I focus on delivering seamless user experiences and visually engaging designs. My goal is to build innovative, high-performance applications that enhance user interaction and drive engagement.`;
 
@@ -9,36 +9,7 @@ export const ABOUT_TEXT = `Hello! I’m Shreyal, a passionate developer and tech
 
 Let’s connect and create something amazing!`;
 
-// export const EXPERIENCES = [
-//   {
-//     year: "2023 - Present",
-//     role: "Senior Full Stack Developer",
-//     company: "Google Inc.",
-//     description: `Led a team in developing and maintaining web applications using JavaScript, React.js, and Node.js. Implemented RESTful APIs and integrated with MongoDB databases. Collaborated with stakeholders to define project requirements and timelines.`,
-//     technologies: ["Javascript", "React.js", "Next.js", "mongoDB"],
-//   },
-//   {
-//     year: "2022 - 2023",
-//     role: "Frontend Developer",
-//     company: "Adobe",
-//     description: `Designed and developed user interfaces for web applications using Next.js and React. Worked closely with backend developers to integrate frontend components with Node.js APIs. Implemented responsive designs and optimized frontend performance.`,
-//     technologies: ["HTML", "CSS", "Vue.js", "mySQL"],
-//   },
-//   {
-//     year: "2021 - 2022",
-//     role: "Full Stack Developer",
-//     company: "Facebook",
-//     description: `Developed and maintained web applications using JavaScript, React.js, and Node.js. Designed and implemented RESTful APIs for data communication. Collaborated with cross-functional teams to deliver high-quality software products on schedule.`,
-//     technologies: ["Python", "Svelte", "Three.js", "Postgres"],
-//   },
-//   {
-//     year: "2020 - 2021",
-//     role: "Software Engineer",
-//     company: "Paypal",
-//     description: `Contributed to the development of web applications using JavaScript, React.js, and Node.js. Managed databases and implemented data storage solutions using MongoDB. Worked closely with product managers to prioritize features and enhancements.`,
-//     technologies: ["Ruby", "Rails", "PHP", "Sqlite"],
-//   },
-// ];
+
 
 export const PROJECTS = [
   {
