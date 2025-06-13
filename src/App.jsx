bfd41ab087d21a8b48aa4666a476d88d12a4a377
Shreyalsinh Raj <131889844+Shreyal216@users.noreply.github.com>
2025-06-13 14:29:05 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,29 @@
+import { useEffect, useRef } from "react";
 import Navbar from "./components/Navbar";
 import Hero from "./components/Hero";
 import AboutMe from "./components/Aboutme";
 import Technologies from "./components/Technologies";
 import Projects from "./components/Projects";
 import Contact from "./components/Contact";
-import bgVideo from "./assets/space.mp4.mp4"
+import bgVideo from "./assets/space.mp4.mp4";
 
 const App = () => {
+  const videoRef = useRef(null);
+
+  useEffect(() => {
+    if (videoRef.current) {
+      videoRef.current.playbackRate = 0.5; 
+    }
+  }, []);
+
   return (
     <div className="overflow-x-hidden text-neutral-300 antialiased selection:bg-cyan-300 selection:text-cyan-900">
       {/* Video Background */}
-      <video 
-        autoPlay 
-        loop 
-        muted 
+      <video
+        ref={videoRef}
+        autoPlay
+        loop
+        muted
         className="fixed inset-0 -z-10 h-full w-full object-cover"
       >
         <source src={bgVideo} type="video/mp4" />
